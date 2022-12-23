1ad961148d208050c1e2b7c94bf01ca4e7799f78
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2022-12-23 18:06:50 +0530

    feat: argument support added (#5)

--- cmd/parse.go
+++ cmd/parse.go
@@ -10,10 +10,11 @@ import (
 func Parse() {
 	uri := flag.String("uri", "", "provide url of package to be installed")
 	version := flag.String("v", "latest", "provide version of the package to be installed, i.e latest")
+	args := flag.String("arg", "", "provide arguments to be provided to package")
 
 	flag.Parse()
 
-	if err := api.InitApi(*uri, *version); err != nil {
+	if err := api.InitApi(*uri, *version, *args); err != nil {
 		log.Fatal(err)
 	}
 }
